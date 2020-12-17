1927949cd438c718c40b7d77b773b51cd10fbe68
rune.laenen <rune.laenen@intracto.com>
2020-12-17 17:49:54 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Image Optimizer for Shopware 6
+
+[![Latest Stable Version](https://img.shields.io/github/v/release/runelaenen/sw6-media-optimizer?color=lightblue&label=stable&logo=github)](//packagist.org/packages/runelaenen/sw6-media-optimizer)
+[![Download plugin zip](https://img.shields.io/github/v/release/runelaenen/sw6-media-optimizer.svg?label=.zip%20download&logo=github)](https://github.com/runelaenen/sw6-media-optimizer/releases/latest)
+[![Total Downloads](https://img.shields.io/packagist/dt/runelaenen/sw6-media-optimizer?label=packagist%20downloads&logo=composer)](//packagist.org/packages/runelaenen/sw6-media-optimizer)
+[![GitHub Issues](https://img.shields.io/github/issues/runelaenen/sw6-media-optimizer?logo=github)](https://github.com/runelaenen/sw6-media-optimizer/issues)
+[![GitHub Stars](https://img.shields.io/github/stars/runelaenen/sw6-media-optimizer?logo=github)](https://github.com/runelaenen/sw6-media-optimizer/stargazers)
+[![License](https://poser.pugx.org/runelaenen/sw6-media-optimizer/license)](//packagist.org/packages/runelaenen/sw6-media-optimizer)
+
+![Image Optimizer for Shopware 6](https://user-images.githubusercontent.com/3930922/102516043-b9b34780-408e-11eb-92f1-f9b0bdf70888.png)
+
+Optimize image files using [spatie/image-optimizer](https://packagist.org/packages/spatie/image-optimizer)
+
+### Optimization tools
+
+The package will use these optimizers if they are present on your system:
+
+- [JpegOptim](http://freecode.com/projects/jpegoptim)
+- [Optipng](http://optipng.sourceforge.net/)
+- [Pngquant 2](https://pngquant.org/)
+- [SVGO](https://github.com/svg/svgo)
+- [Gifsicle](http://www.lcdf.org/gifsicle/)
+- [cwebp](https://developers.google.com/speed/webp/docs/precompiled)
+
+Please take a look at [the README of spatie/image-optimizer](https://packagist.org/packages/spatie/image-optimizer) to find out how to install these optimizers on your system.
+
+## How to install
+### Composer install (recommended)
+```
+composer require runelaenen/sw6-media-optimizer
+bin/console plugin:refresh
+bin/console plugin:install --activate RuneLaenenMediaOptimizer
+```
+
+You can now run the optimizer
+```
+bin/console rl:media:optimize
+```
+
+Tip: use the `--info` option to show all enabled optimizers.
+
+#### Plugin update (composer)
+```
+composer update runelaenen/sw6-media-optimizer
+bin/console plugin:update RuneLaenenMediaOptimizer
+```
+
+### .zip install
+1. Download the latest RuneLaenenMediaOptimizer.zip from the [latest release](https://github.com/runelaenen/sw6-media-optimizer/releases/latest).
+2. Upload the zip in the Shopware Administration
+3. Install & Activate the plugin
+
+#### Plugin update (zip)
+1. Download the latest RuneLaenenMediaOptimizer.zip from the [latest release](https://github.com/runelaenen/sw6-media-optimizer/releases/latest).
+2. Upload the zip in the Shopware Administration
+3. Update the plugin
---
+++ src/Command/MediaOptimizeCommand.php
@@ -0,0 +1,170 @@
+<?php declare(strict_types=1);
+
+namespace RuneLaenen\MediaOptimizer\Command;
+
+use Shopware\Core\Content\Media\MediaEntity;
+use Shopware\Core\Content\Media\Pathname\UrlGeneratorInterface;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\RepositoryIterator;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Spatie\ImageOptimizer\OptimizerChain;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MediaOptimizeCommand extends Command
+{
+    protected static $defaultName = 'rl:media:optimize';
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $mediaRepository;
+
+    /**
+     * @var OptimizerChain
+     */
+    private $optimizerChain;
+
+    /**
+     * @var int
+     */
+    private $batchSize;
+
+    /**
+     * @var int
+     */
+    private $sizePre = 0;
+
+    /**
+     * @var int
+     */
+    private $sizePost = 0;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * @var string
+     */
+    private $projectDir;
+
+    public function __construct(
+        EntityRepositoryInterface $mediaRepository,
+        OptimizerChain $optimizerChain,
+        UrlGeneratorInterface $urlGenerator,
+        string $projectDir
+    ) {
+        parent::__construct();
+        $this->mediaRepository = $mediaRepository;
+        $this->optimizerChain = $optimizerChain;
+        $this->urlGenerator = $urlGenerator;
+        $this->projectDir = $projectDir;
+    }
+
+    public function configure(): void
+    {
+        $this->addOption('info')
+            ->addOption(
+                'batch-size',
+                'b',
+                InputOption::VALUE_REQUIRED,
+                'Number of entities per iteration',
+                '50'
+            );
+    }
+
+    public function run(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('info')) {
+            $optimizers = $this->optimizerChain->getOptimizers();
+
+            $rows = [];
+            foreach ($optimizers as $optimizer) {
+                $rows[] = [
+                    $optimizer->binaryName(),
+                ];
+            }
+
+            $table = new Table($output);
+            $table->setHeaders([
+                'Optimizer',
+            ])
+            ->setRows($rows);
+            $table->render();
+
+            return 0;
+        }
+        $this->batchSize = $this->getBatchSizeFromInput($input);
+        $context = Context::createDefaultContext();
+
+        $mediaIterator = new RepositoryIterator($this->mediaRepository, $context, $this->createCriteria());
+        $totalMediaCount = $mediaIterator->getTotal();
+
+        $progressBar = new ProgressBar($output, $totalMediaCount);
+        $progressBar->start();
+
+        while (($result = $mediaIterator->fetch()) !== null) {
+            foreach ($result->getEntities() as $media) {
+                $this->optimize($media, $context);
+                $progressBar->advance();
+            }
+        }
+
+        $progressBar->finish();
+
+        $output->writeln([
+            '',
+            'Filesize (bytes) before optimalization: ' . $this->sizePre,
+            'Filesize (bytes) after optimalization: ' . $this->sizePost,
+            'Bytes saved: ' . ($this->sizePre - $this->sizePost) . ' (' . (round((1 - $this->sizePre / $this->sizePost) * 100, 2)) . '%)',
+        ]);
+
+        return 0;
+    }
+
+    private function getBatchSizeFromInput(InputInterface $input): int
+    {
+        $rawInput = $input->getOption('batch-size');
+
+        if (\is_array($rawInput) || !is_numeric($rawInput)) {
+            throw new \UnexpectedValueException('Batch size must be numeric');
+        }
+
+        return (int) $rawInput;
+    }
+
+    private function createCriteria(): Criteria
+    {
+        $criteria = new Criteria();
+        $criteria->setOffset(0);
+        $criteria->setLimit($this->batchSize);
+        $criteria->addFilter(new EqualsFilter('media.mediaFolder.configuration.createThumbnails', true));
+        $criteria->addAssociation('thumbnails');
+        $criteria->addAssociation('mediaFolder.configuration.mediaThumbnailSizes');
+
+        return $criteria;
+    }
+
+    private function optimize(MediaEntity $media): void
+    {
+        $mediaLocation = $this->projectDir . '/public/' . $this->urlGenerator->getRelativeMediaUrl($media);
+        if (!file_exists($mediaLocation)) {
+            return;
+        }
+        $sizePre = filesize($mediaLocation);
+        $this->sizePre += $sizePre;
+
+        $this->optimizerChain->optimize($mediaLocation);
+
+        $sizePost = filesize($mediaLocation);
+        $this->sizePost += $sizePost;
+    }
+}

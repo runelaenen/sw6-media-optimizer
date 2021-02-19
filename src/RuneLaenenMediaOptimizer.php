7961ce72ca8037c7f432f014137e79e6e444a6e3
rune.laenen <rune.laenen@intracto.com>
2021-02-19 17:13:42 +0100

    Add autoloader

--- src/RuneLaenenMediaOptimizer.php
+++ src/RuneLaenenMediaOptimizer.php
@@ -4,6 +4,14 @@ namespace RuneLaenen\MediaOptimizer;
 
 use Shopware\Core\Framework\Plugin;
 
+if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    $loader = require_once dirname(__DIR__) . '/vendor/autoload.php';
+    if ($loader !== true) {
+        spl_autoload_unregister([$loader, 'loadClass']);
+        $loader->register(false);
+    }
+}
+
 class RuneLaenenMediaOptimizer extends Plugin
 {
 }

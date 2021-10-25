85ef6fdf3dc23d3cc1cc133679be6d7a4d4a0935
Brent Robert <brent.robert@phpro.be>
2021-10-25 16:59:25 +0200

    Added GrumPHP workflow

--- src/RuneLaenenMediaOptimizer.php
+++ src/RuneLaenenMediaOptimizer.php
@@ -4,8 +4,8 @@ namespace RuneLaenen\MediaOptimizer;
 
 use Shopware\Core\Framework\Plugin;
 
-if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
-    $loader = require_once dirname(__DIR__) . '/vendor/autoload.php';
+if (file_exists(\dirname(__DIR__) . '/vendor/autoload.php')) {
+    $loader = require_once \dirname(__DIR__) . '/vendor/autoload.php';
     if ($loader !== true) {
         spl_autoload_unregister([$loader, 'loadClass']);
         $loader->register(false);
